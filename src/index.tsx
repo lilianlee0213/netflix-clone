a0e66f2243e93179ef3fc36dc5fb79ebb7b24287
Lilian Lee <lilianlee0213@gmail.com>
2023-04-20 20:32:40 -0400

    home screen pt.1: get api

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import {RecoilRoot} from 'recoil';
 import {createGlobalStyle, ThemeProvider} from 'styled-components';
 import {theme} from './theme';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -75,14 +76,17 @@ input{
 
 }
 `;
+const client = new QueryClient();
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<RecoilRoot>
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<App />
-		</ThemeProvider>
+		<QueryClientProvider client={client}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<App />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</RecoilRoot>
 );
