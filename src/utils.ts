8e33d3e8628c424fa070d3f71bde1aa295a934c1
Lilian Lee <lilianlee0213@gmail.com>
2023-06-27 21:58:16 -0400

    security site

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
-export const netflixLogoUrl =
-	'https://images.ctfassets.net/y2ske730sjqp/2e4W8sQrJTX5i1wceNpCSE/1b34a6c6fe12ce9fb7553d33cf6b80f2/BrandAssets_Logos_07-Avoid_01-NSymbol_03.jpg?w=500';
+export const netflixLogoUrl = 'https://images/text';
 
 export function makeImagePath(id: string, format?: string) {
 	return `https://image.tmdb.org/t/p/${format ? format : 'original'}/${id}`;
 }
+// 'https://images.ctfassets.net/y2ske730sjqp/2e4W8sQrJTX5i1wceNpCSE/1b34a6c6fe12ce9fb7553d33cf6b80f2/BrandAssets_Logos_07-Avoid_01-NSymbol_03.jpg?w=500'
